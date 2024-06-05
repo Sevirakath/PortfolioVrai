7f5f3bcdb9410292bd282711045cbf17a5d235c9
Sevirakath <tomstef34@gmail.com>
2024-06-05 20:31:17 +0200

    portfolio

--- Portfolio vrai/Portfolio/script.js
+++ Portfolio vrai/Portfolio/script.js
@@ -1,15 +1,43 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const projets = document.querySelectorAll('.projet');
+    const navLinks = document.querySelectorAll('nav ul li a');
 
-    projets.forEach(projet => {
-        projet.addEventListener('mouseover', function () {
-            const description = this.querySelector('.project-description');
-            description.style.opacity = '1';
-        });
+    navLinks.forEach(link => {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href');
+            const targetSection = document.querySelector(targetId);
 
-        projet.addEventListener('mouseout', function () {
-            const description = this.querySelector('.project-description');
-            description.style.opacity = '0';
+            // Masquer toutes les sections
+            document.querySelectorAll('section').forEach(section => {
+                section.classList.remove('section-visible');
+            });
+
+            // Supprimer la classe de la section cible pour la rendre visible avec un effet de "slide"
+            targetSection.classList.add('section-visible');
         });
+    });
+
+    // Ajouter et colorer les barres de progression
+    const skills = document.querySelectorAll('.competence');
+
+    skills.forEach(skill => {
+        const percentage = parseInt(skill.getAttribute('data-percentage'));
+        const progressBar = document.createElement('div');
+        progressBar.className = 'progress-bar';
+
+        const progress = document.createElement('div');
+        progress.className = 'progress';
+        progress.style.width = percentage + '%';
+
+        if (percentage < 25) {
+            progressBar.classList.add('rouge');
+        } else if (percentage < 50) {
+            progressBar.classList.add('orange');
+        } else {
+            progressBar.classList.add('vert');
+        }
+
+        progressBar.appendChild(progress);
+        skill.appendChild(progressBar);
     });
 });
